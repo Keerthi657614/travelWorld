78cf56f2dba48a697fab6742fba29179007b0423
Keerthi657614 <147122327+Keerthi657614@users.noreply.github.com>
2025-02-02 13:35:00 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://travelworldbackend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
